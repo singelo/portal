e37ded8e20e54e8a65e1e9995bd776f4df5975bf
Singelo <61355326+singelo@users.noreply.github.com>
2026-03-22 23:34:53 -0300

    add js api and app

---
+++ assets/js/api.js
@@ -0,0 +1,54 @@
+const API_URL = 'https://script.google.com/macros/s/AKfycbz5U38Rr5SplH74c8Jg9A4HES1WZgj_w_9sYEX-EA7A11yco53wTtqznkfqkKItwAWh6Q/exec';
+
+async function apiRequest(action, payload = {}, auth = true) {
+
+  const token = sessionStorage.getItem("rm_token");
+
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      action,
+      token: auth ? token : "",
+      payload
+    })
+  });
+
+  const data = await res.json();
+
+  if (!data.ok) {
+    throw new Error(data.error || "Erro na API");
+  }
+
+  return data.data;
+}
+
+async function login(password) {
+
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      action: "login",
+      password
+    })
+  });
+
+  const data = await res.json();
+
+  if (!data.ok) {
+    throw new Error(data.error || "Erro no login");
+  }
+
+  sessionStorage.setItem("rm_token", data.data.token);
+
+  return true;
+}
+
+function logout() {
+  sessionStorage.removeItem("rm_token");
+}
